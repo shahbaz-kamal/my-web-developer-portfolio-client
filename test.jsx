3d1a11b4426f78daaca0fc663d4ff0a1f36eedf4
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-01-04 00:48:32 +0600

    project setup done

---
+++ test.jsx
@@ -0,0 +1,33 @@
+
+
+
+Dark option 1
+
+colors: {
+    'text': '#f5fafa',
+    'background': '#05100f',
+    'primary': '#25d4bd',
+    'secondary': '#0b796c',
+    'accent': '#02b19c',
+   },
+   
+
+   light 1
+   colors: {
+    'text': '#100c0d',
+    'background': '#fcfafa',
+    'primary': '#c83a4e',
+    'secondary': '#eb8794',
+    'accent': '#f5445c',
+   },
+   dark final
+
+   colors: {
+    'text': '#e3e5f4',
+    'background': '#050712',
+    'primary': '#919bea',
+    'secondary': '#122197',
+    'accent': '#354cf5',
+   },
+   
+   
