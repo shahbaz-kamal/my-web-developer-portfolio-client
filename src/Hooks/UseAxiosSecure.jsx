2e46672947942ab3eb53a3e9daea29d88f4f67f5
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-02-06 02:16:34 +0600

    will build now

--- src/Hooks/UseAxiosSecure.jsx
+++ src/Hooks/UseAxiosSecure.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/",
-  // baseURL: "https://my-portfolio-server-mauve.vercel.app/",
+  // baseURL: "http://localhost:5000/",
+  baseURL: "https://my-portfolio-server-mauve.vercel.app/",
 });
 const UseAxiosSecure = () => {
   return axiosInstance;
