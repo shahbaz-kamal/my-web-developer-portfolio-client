db8a6c6e21debccd38dd28413f117a00f2911fba
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-03-12 22:31:21 +0600

    changed educational qualification

--- src/Pages/Home/Education.jsx
+++ src/Pages/Home/Education.jsx
@@ -10,14 +10,14 @@ const Education = () => {
         <Title title={"Education"} subTitle={"My Academic Path"}></Title>
       </header>
       <section className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6">
-        <Fade triggerOnce={true} direction="left">
+        {/* <Fade triggerOnce={true} direction="left">
           <EducationCard
             degree="M.Sc.(Engg.) in Electrical & Electronic Engineering"
             institution={"Bangladesh University of Engineering and Technology"}
             timeline={"2021-Present"}
             gpa={"3.50"}
           ></EducationCard>
-        </Fade>{" "}
+        </Fade> */}
         <Fade triggerOnce={true} direction="right">
           <EducationCard
             degree="B.Sc. in Electrical & Electronic Engineering"
